592e540c35de5ed5410bc848084d7ee3b6a98a0f
FALVO Anthony <anthony.falvo.pro@gmail.com>
2021-07-12 07:58:46 +0200

    menu collant ok, creation du fake element pour eviter le decallage

--- source/js/index.js
+++ source/js/index.js
@@ -1,7 +1,44 @@
 (function(){
 
+    // variable
 
-    
+    // recupere mon menu
+    let menu = document.querySelector('.menu');
+    // comme scrollY mais permet une compatibilité navigateur plus global 
+    let scrollY = function() {
+
+        let supportPageOffset = window.pageXOffset !== undefined;
+        let isCSS1Compat = ((document.compatMode || "") === "CSS1Compat");
+
+        return supportPageOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop;
+
+    };
+    // fonction qui permet de recuperer le scroll de lelement et le scroolY deja effectuer
+    let top = menu.getBoundingClientRect().top + scrollY();
+    // dans lexemple du cours, sont element perd ca largeur, j'ai pas ce probleme avec le flex, mais je vais utilisé ca methode au cas ou si je venais a en avoir besoin 
+    let witdh = menu.getBoundingClientRect().witdh;
+
+
+
+    // ma condition si le nombre de scroll depasse selui de lelement alors, la position ce fixe, sinon elle s'enleve
+    let onScroll = () => {
+
+        // permet de voir si  j'ai deja cette classe
+        let aTilScroller = menu.classList.contains('fixed');
+
+        if (scrollY() > top && !aTilScroller) {
+
+            menu.classList.add('fixed')
+            menu.style.witdh = witdh + "px";
+
+        } else if(scrollY() < top && aTilScroller) {
+
+            menu.classList.remove('fixed')
+
+        }
+    };
+
+    window.addEventListener('scroll', onScroll);
 
 
 })();
