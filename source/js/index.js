7d6667180c4cb4de797f865eaee9f717ac14433c
FALVO Anthony <anthony.falvo.pro@gmail.com>
2021-07-12 08:24:54 +0200

    js adjust

--- source/js/index.js
+++ source/js/index.js
@@ -1,9 +1,6 @@
 (function(){
 
-    // variable
-
-    // recupere mon menu
-    let menu = document.querySelector('.menu');
+    // VARIABLE
     // comme scrollY mais permet une compatibilité navigateur plus global 
     let scrollY = function() {
 
@@ -13,12 +10,23 @@
         return supportPageOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop;
 
     };
+
+    // recupere mon menu
+    let menu = document.querySelector('.menu');
     // fonction qui permet de recuperer le scroll de lelement et le scroolY deja effectuer
     let top = menu.getBoundingClientRect().top + scrollY();
     // dans lexemple du cours, sont element perd ca largeur, j'ai pas ce probleme avec le flex, mais je vais utilisé ca methode au cas ou si je venais a en avoir besoin 
     let witdh = menu.getBoundingClientRect().witdh;
+    // je cree une variable qui serra mon menu
+    let rectangle = menu.getBoundingClientRect();
+    // je cree un faux menu, pour remplacer celui qui serra fixé, ainsi evité le decallage 
+    let fake = document.createElement('div');
+    // je lui donne la largeur et la hauteur de mon menu
+    fake.style.width = rectangle.width + "px";
+    fake.style.height = rectangle.height + "px";
 
 
+    // FONCTION
 
     // ma condition si le nombre de scroll depasse selui de lelement alors, la position ce fixe, sinon elle s'enleve
     let onScroll = () => {
@@ -30,15 +38,33 @@
 
             menu.classList.add('fixed')
             menu.style.witdh = witdh + "px";
+            // j'ajoute mon faux menu 
+            menu.parentNode.insertBefore(fake, menu);
 
         } else if(scrollY() < top && aTilScroller) {
 
-            menu.classList.remove('fixed')
-
+            menu.classList.remove('fixed');
+            menu.parentNode.removeChild(fake);
         }
     };
 
-    window.addEventListener('scroll', onScroll);
+    // si la page est redimensionner lelement fake a besoin detre adapter en largeur hauteur . cette fonction va permettre de le faire
+    let onResize = function() {
 
+        menu.style.width = "auto"
+        menu.classList.remove('fixed');
+        fake.style.display = "none"
+        rectangle = menu.getBoundingClientRect()
+        top = rectangle.top + scrollY()
+        fake.style.width = rectangle.width + "px"
+        fake.style.height = rectangle.height + "px"
+        fake.style.display = "block"
+        onScroll()
+
+    }
+
+    // LISTENER
+    window.addEventListener('scroll', onScroll);
+    window.addEventListener('resize', onResize);
 
 })();
